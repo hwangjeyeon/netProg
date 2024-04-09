f22583cbbae73e60154709367e5a54a25a62cbd4
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 12:07:55 +0900

    연습문제 10

--- practice1.py
+++ practice1.py
@@ -1,23 +1,9 @@
-d = [{'name':'Todd','phone': '555-1414', 'email' : 'todd@mail.net'},
-     {'name' : 'Helga', 'phone': '555-1618', 'email':'helga@mail.net'},
-     {'name':'princess','phone':'555-3141', 'email' : ''},
-     {'name':'LJ','phone':'555-2718','email':'lj@mail.net'}
-     ]
-for i in d:
-    if i['phone'].endswith('8'):
-        print(i['name'])
+s = input("문자열을 입력하세요")
+a = s.split('&')
 
-for i in d:
-    if not i['email']:
-        print(i['name'])
+dict = {}
+for i in a:
+    b, c = i.split('=')
+    dict[b.strip()] = c.strip()
 
-name = input("이름을 입력하세요")
-isFind = False
-for i in d:
-    if i['name'] == name:
-        print(i['phone'])
-        print(i['email'])
-        isFind = True
-        break
-if not isFind:
-    print("이름이 없습니다.")
+print(dict)
