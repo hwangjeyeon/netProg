551679b9f48b7b6f61f7909e1162d9f0bba12f32
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 11:30:24 +0900

    연습문제2 풀이

--- practice1.py
+++ practice1.py
@@ -1,8 +1,14 @@
-list = ["hwang", "yoo", "kwon"]
+list = [1,2,3]
 print(list)
-list.insert(0, "kim")
+list[1] = 17
 print(list)
-list.insert(2, "hong")
+list.extend([4,5,6])
 print(list)
-list.append("yun")
+list.pop(0)
+print(list)
+list.sort()
+print(list)
+list.reverse()
+print(list)
+list[3] = 25
 print(list)
