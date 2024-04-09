8ac73d34cf7d901b41923f128e4b7e68151cae44
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 11:42:43 +0900

    연습문제 5

--- practice1.py
+++ practice1.py
@@ -1,16 +1,5 @@
+word = input("Your Word: ")
 
-a = int(input("첫번째 수를 입력하세요: "))
-b = int(input("두번째 수를 입력하세요: "))
-
-if(a < b):
-    tmp = a
-    a = b
-    b = tmp
-
-while b!=0:
-    n = a%b
-    a = b
-    b = n
-
-print(a)
-
+finds = word.find("a")
+print(word[:finds+1])
+print(word[finds+1:])
