0d821f6e8e0e94c60c9b65a13100d14740279410
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 11:49:12 +0900

    연습문제 7

--- practice1.py
+++ practice1.py
@@ -1,14 +1,9 @@
-a = int(input("첫번째 수를 입력하세요: "))
-b = int(input("두번째 수를 입력하세요: "))
+import math
+list1 = []
+list2 = []
+for i in range(0,50):
+    list1.append(i)
+    list2.append(int(math.pow(i, 2)))
 
-if(a < b):
-    tmp = a
-    a = b
-    b = tmp
-
-while b!=0:
-    n = a%b
-    a = b
-    b = n
-
-print(a)
+print(list1)
+print(list2)
