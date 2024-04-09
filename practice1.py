b704455bbb7e77a3de8858d2878025d3dfb88b0b
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 11:59:15 +0900

    연습문제 8

--- practice1.py
+++ practice1.py
@@ -1,9 +1,27 @@
-import math
-list1 = []
-list2 = []
-for i in range(0,50):
-    list1.append(i)
-    list2.append(int(math.pow(i, 2)))
+days = {'January':31, 'February':28, 'March':31, 'April':30,
+'May':31, 'June':30, 'July':31, 'August':31,
+'September':30, 'October':31, 'November':30,
+'December':31}
 
-print(list1)
-print(list2)
+month = input("월을 입력하세요")
+if month in days:
+    print(days[month])
+else:
+    print("해당하는 월이 없습니다")
+
+for i in sorted(days.keys()):
+    print(i)
+
+print()
+for i, j in days.items():
+    if j == 31:
+        print(i)
+print()
+for i, j in sorted(days.items(), key=lambda x: x[1]):
+    print(i, j)
+
+month = input("월을 3자리만 입력하세요")
+for i, j in days.items():
+    if i.startswith(month):
+        print(j)
+
