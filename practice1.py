a1a09a77c1bf585e2bc91b4ea3a768fe73015e05
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-09 12:11:54 +0900

    연습문제 11

--- practice1.py
+++ practice1.py
@@ -1,9 +1,22 @@
-s = input("문자열을 입력하세요")
-a = s.split('&')
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-dict = {}
-for i in a:
-    b, c = i.split('=')
-    dict[b.strip()] = c.strip()
+    def getName(self):
+        return self.name
 
-print(dict)
+    def getAge(self):
+        return self.age
+
+class Employee(Person):
+    def __init__(self, name, age, id):
+        self.name = name
+        self.age = age
+        self.id = id
+    def getId(self):
+        return self.id
+
+
+a = Employee("IoT", 65, 2018)
+print(a.getName(), a.getAge(), a.getId())
